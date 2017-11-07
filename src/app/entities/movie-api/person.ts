10f981c3d7a5d19a2c8fa457e2481c67abfa499c
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-11-07 10:35:22 +0100

    Ajout Person

---
+++ src/app/entities/movie-api/person.ts
@@ -0,0 +1,9 @@
+export class Person {
+  name : string;
+  firstname : string;
+  birthDate : string;
+  deathDate : string;
+  biography : string;
+  children : Array<Person>;
+  spouse : Array<Person>;
+}
