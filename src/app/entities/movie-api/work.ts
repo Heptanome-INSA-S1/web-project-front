9c7e25cb3bd87a80816a3abc8720fc254fccdd84
MrJuju97351 <justin.gallysp@gmail.com>
2017-11-07 11:39:01 +0100

    Work entitie

---
+++ src/app/entities/movie-api/work.ts
@@ -0,0 +1,33 @@
+export class Work {
+  constructor(
+    public  uri: Array<URI>,
+    public poster: string,
+    public title: string,
+    public releaseDate: string,
+    public plot: string,
+    public runtime: number,
+    public actors: Array<Artist>,
+    public genres: Array<string>,
+    public director: Array<Artist>,
+    public gross: number,
+    public budget: number
+  ) {}
+}
+
+
+/*
+Work {
+    uri : Array<URI> | Liens des ressources,
+    poster : string | Liens de l’affiche,
+    title : string | Titre en français en priorité sinon anglais,
+    releaseDate: Date | Date d’apparition,
+    plot: string | Résumé,
+    runtime : integer,
+    actors: Array<Artist>, | Casting
+genres : Array<string> | Genres
+    director: Array<Artist> | Réalisateurs,
+gross: double | Revenus,
+    budget: double | Budget
+}
+
+ */
