093e69137b2c64fe0af4c2ccf9018e2a1b914883
ThomasPoint <pointthomas84700@gmail.com>
2017-11-07 12:06:35 +0100

    add artist entity

---
+++ src/app/entities/movie-api/artist.ts
@@ -0,0 +1,18 @@
+import {Movie} from './movie';
+import {Person} from './person';
+
+export class Artist extends Person{
+  constructor(
+    name: string,
+    firstname: string,
+    birthDate: string,
+    deathDate: string,
+    biography: string,
+    children: Array<Person>,
+    spouse: Array<Person>,
+    public filmography: Array<Movie>,
+    public bestMovies : Array<Movie>
+  ){
+    super(name,firstname,birthDate, deathDate,biography,children,spouse);
+  }
+}
