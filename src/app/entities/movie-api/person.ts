7dbc68d5cdb0dff9602c18e5ae587f39c990c887
MrJuju97351 <justin.gallysp@gmail.com>
2017-11-07 12:03:19 +0100

    .

--- src/app/entities/movie-api/person.ts
+++ src/app/entities/movie-api/person.ts
@@ -1,9 +1,11 @@
 export class Person {
-  public name: string;
-  public firstname: string;
-  public birthDate: string;
-  public deathDate: string;
-  public biography: string;
-  public children: Array<Person>;
-  public spouse: Array<Person>;
+  constructor(
+    public name: string,
+    public firstname: string,
+    public birthDate: string,
+    public deathDate: string,
+    public biography: string,
+    public children: Array<Person>,
+    public spouse: Array<Person>
+  ) {}
 }
