6cc4e4875f881b0ef1df66ea0c62387ccd15d7b8
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-11-09 19:28:15 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/components/movie-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-detail',
+  templateUrl: '../html/movie-detail.component.html'
+})
+export class MovieDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
