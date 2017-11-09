5f822e9ed9e7661eb9b0599ede4b03723b530fb3
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-11-09 20:03:26 +0100

    Initial version of MovieDetailComponent

--- src/app/components/movie-detail.component.ts
+++ src/app/components/movie-detail.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import {MovieService} from '../services/movie.service';
 import {Work} from '../entities/movie-api/work';
+import {ActivatedRoute} from '@angular/router';
+import {AlertService} from '../services/alert.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: '../html/movie-detail.component.html',
-  providers: [MovieService]
+  providers: [MovieService, AlertService],
 })
 
 export class MovieDetailComponent implements OnInit {
   private movie: Work;
+  private charged: boolean;
   constructor(
-    private movieService: MovieService
+    private movieService: MovieService,
+    private alertService: AlertService,
+    private route: ActivatedRoute,
+    private location: Location
   ) { }
 
   ngOnInit() {
-    this.movieService
+      this.getMovie();
+  }
+
+  getMovie(): void {
+    this.charged = false;
+    const id = this.route.snapshot.paramMap.get('id');
+    this.movieService.getMovieByResource(id)
+      .then(res => {
+        this.movie = res;
+        this.charged = true;
+      })
+      .catch(res => {
+        this.charged = true;
+        this.alertService.warn('Error encountered :', 'Error encountered with the tv-show API', 'warning');
+      });
   }
 
 }
