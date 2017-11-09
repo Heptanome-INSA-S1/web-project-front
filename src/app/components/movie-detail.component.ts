896988bfee0766681a785944cd87862444728c86
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2017-11-09 21:11:06 +0100

    Bug correction

--- src/app/components/movie-detail.component.ts
+++ src/app/components/movie-detail.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import {MovieService} from '../services/movie.service';
 import {Work} from '../entities/movie-api/work';
@@ -9,33 +10,40 @@ import {AlertService} from '../services/alert.service';
   templateUrl: '../html/movie-detail.component.html',
   providers: [MovieService, AlertService],
 })
-
 export class MovieDetailComponent implements OnInit {
+
   private movie: Work;
   private charged: boolean;
+
   constructor(
     private movieService: MovieService,
     private alertService: AlertService,
-    private route: ActivatedRoute,
-    private location: Location
-  ) { }
+    private route: ActivatedRoute
+) { }
 
-  ngOnInit() {
-      this.getMovie();
+
+
+ngOnInit() {
+  this.getMovie();
+}
+
+
+
+getMovie(): void {
+
+  this.charged = false;
+  const id = this.route.snapshot.paramMap.get('id');
+  this.movieService.getMovieByResource(id)
+    .then(res => {
+      this.movie = res;
+      this.charged = true;
+    })
+    .catch(res => {
+      this.charged = true;
+      this.alertService.warn('Error encountered :', 'Error encountered with the tv-show API', 'warning');
+    });
   }
 
-  getMovie(): void {
-    this.charged = false;
-    const id = this.route.snapshot.paramMap.get('id');
-    this.movieService.getMovieByResource(id)
-      .then(res => {
-        this.movie = res;
-        this.charged = true;
-      })
-      .catch(res => {
-        this.charged = true;
-        this.alertService.warn('Error encountered :', 'Error encountered with the tv-show API', 'warning');
-      });
-  }
+
 
 }
