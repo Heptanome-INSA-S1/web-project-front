e3f3346be6bc66e4e9f718867251015794344576
ThomasPoint <pointthomas84700@gmail.com>
2017-11-10 09:42:55 +0100

    update actor-detail.component.ts

--- src/app/components/actor-detail.component.ts
+++ src/app/components/actor-detail.component.ts
@@ -3,16 +3,18 @@ import {ActorService} from '../services/actor.service';
 import {ActivatedRoute} from '@angular/router';
 import {AlertService} from '../services/alert.service';
 import {Artist} from '../entities/movie-api/artist';
+import {Person} from '../entities/movie-api/person';
+import {ACTOR_EXEMPLE} from '../app.constants';
 
 @Component({
-  selector: 'app-movie-detail',
+  selector: 'app-actor-detail',
   templateUrl: '../html/actor-detail.component.html',
   providers: [ActorService],
 })
 
 
 export class ActorDetailComponent implements OnInit {
-  private actor: Artist;
+  private actor: Artist = ACTOR_EXEMPLE;
   private charged: boolean;
 
   constructor(
@@ -22,7 +24,11 @@ export class ActorDetailComponent implements OnInit {
   ){}
 
   ngOnInit() {
-      this.getActor();
+      //this.getActor();
+  }
+
+  link(person: Person): string {
+    return ActorService.parseUuidActor(person);
   }
 
   getActor(): void {
